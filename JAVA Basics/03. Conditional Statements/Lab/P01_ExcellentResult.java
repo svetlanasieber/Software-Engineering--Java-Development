fc5fd20cdec66a044fc18ac766e14dc7e9c0b593
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-09-14 10:33:05 +0200

    Create P01_ExcellentResult.java

---
+++ JAVA Basics/03. Conditional Statements/Lab/P01_ExcellentResult.java
@@ -0,0 +1,16 @@
+package L3_ConditionalStatements_Lab;
+
+import java.util.Scanner;
+
+public class P01_ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int grade = Integer.parseInt(scanner.nextLine());
+        
+        if (grade >= 5) {
+            System.out.println("Excellent!");
+        }
+
+    }
+}
