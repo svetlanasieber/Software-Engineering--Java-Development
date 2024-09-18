b43ef77dd1f0438330e3cb221ed8c2b6409906c9
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-09-19 01:35:43 +0200

    Create P02_Nums1To10.java

---
+++ JAVA Basics/01. First Steps In Coding/P02_Nums1To10.java
@@ -0,0 +1,19 @@
+package L01_FirstStepsInCoding;
+
+import java.util.Scanner;
+
+public class P02_Nums1To10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+        System.out.println(6);
+        System.out.println(7);
+        System.out.println(8);
+        System.out.println(9);
+        System.out.println(10);
+    }
+}
