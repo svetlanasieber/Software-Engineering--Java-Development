5b5707db7fd7f88a0e51d5bf3c4509319008d163
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-01 18:09:40 +0200

    Delete JAVA DB/MySQL/16. Exercise: Functions and Procedures/01. Employees with Salary Above 35000.sql

--- JAVA DB/MySQL/16. Exercise: Functions and Procedures/01. Employees with Salary Above 35000.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE usp_get_employees_salary_above_35000()
-BEGIN
-
-    SELECT `first_name`, `last_name` FROM employees
-    WHERE `salary` > 35000
-    ORDER BY first_name, last_name, employee_id;
-
-ЕND
