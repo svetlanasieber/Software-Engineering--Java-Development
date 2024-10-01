8a6a8ddbe10a43f7a231d999a8b641f1760e6747
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-01 18:10:06 +0200

    Delete JAVA DB/MySQL/16. Exercise: Functions and Procedures directory

--- JAVA DB/MySQL/16. Exercise: Functions and Procedures/03. Town Names Starting With.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE  usp_get_towns_starting_with(town_name VARCHAR(50))
-BEGIN
-    SELECT `name` FROM towns
-        WHERE `name` LIKE CONCAT(town_name,'%')
-    ORDER BY `name`;
-end
--- JAVA DB/MySQL/16. Exercise: Functions and Procedures/08. Find Full Name.sql
+++
@@ -1,5 +0,0 @@
-CREATE PROCEDURE  usp_get_holders_full_name()
-BEGIN
-    SELECT CONCAT(`first_name`,' ', `last_name`) AS `full_name` FROM account_holders
-        ORDER BY `full_name`, id;
-end
