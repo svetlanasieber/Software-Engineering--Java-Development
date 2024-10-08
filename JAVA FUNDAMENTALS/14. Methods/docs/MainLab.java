ce5e2cd621374737653f9ffa2fac2db3b6284124
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-08 20:19:36 +0200

    Create MainLab.java

---
+++ JAVA FUNDAMENTALS/14. Methods/docs/MainLab.java
@@ -0,0 +1,21 @@
+package Methods_Lab;
+
+public class MainLab {
+    public static void main(String[] args) {
+
+        String[] names = new String[5];
+        names[0] = "Vik";
+        names[1] = "Desi";
+        names[2] = "Gosho";
+        names[3] = "Ivan";
+        print(names);
+    }
+
+    private static void print(String[] names) {
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+
+}
