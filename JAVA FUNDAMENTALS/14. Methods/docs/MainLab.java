53020f99fc7800e535301c18b99ccc82cc0fd519
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-08 20:26:04 +0200

    Update MainLab.java

--- JAVA FUNDAMENTALS/14. Methods/docs/MainLab.java
+++ JAVA FUNDAMENTALS/14. Methods/docs/MainLab.java
@@ -3,16 +3,21 @@ package Methods_Lab;
 public class MainLab {
     public static void main(String[] args) {
 
+        //A:
         String[] names = new String[5];
         names[0] = "Vik";
         names[1] = "Desi";
         names[2] = "Gosho";
         names[3] = "Ivan";
         names[4] = "Raja";
-        print(names);
-    }
+        //print(names);
 
-    private static void print(String[] names) {
+        //1. Varargs =
+        //print();
+        print("Vik", "Ivan", "Gosho", "Ivan", "Raja");
+    }
+//B: VarArgs (String... names)
+    private static void print(String... names) {
 
         for (String name : names) {
             System.out.println(name);
