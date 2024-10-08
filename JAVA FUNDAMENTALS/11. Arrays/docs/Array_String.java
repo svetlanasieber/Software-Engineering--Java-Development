748db87580d848557738e5ceaa237033ae1ab6ce
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-08 16:45:26 +0200

    Update Array_String.java

--- JAVA FUNDAMENTALS/11. Arrays/docs/Array_String.java
+++ JAVA FUNDAMENTALS/11. Arrays/docs/Array_String.java
@@ -2,10 +2,10 @@ package Arrays_Lab;
 
 public class Array_String {
     public static void main(String[] args) {
-        String[] names = new String[10]; //[null, null, null, null, null, null, null, null, null, null] -> default value 
-        names[0] = "Desi";
-        names[1] = "Ivan";
-        names[2] = "Miroslav";
+        String[] names = new String[10]; //[null, null, null, null, null, null, null, null, null, null]
+        names[0] = "Desi";//["Desi", null, null, null, null, null, null, null, null, null]
+        names[1] = "Ivan";//["Desi", "Ivan", null, null, null, null, null, null, null, null]
+        names[2] = "Miroslav";//["Desi", "Ivan", "Miroslav", null, null, null, null, null, null, null]
         //valid positions: >= 0 and < length
         System.out.println(names[8]);
     }
