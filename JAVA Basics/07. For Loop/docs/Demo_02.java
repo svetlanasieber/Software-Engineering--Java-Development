b716e8747d5c415cf1f6f38accce8b8133d60205
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-08 13:03:18 +0200

    Create Demo_02.java

---
+++ JAVA Basics/07. For Loop/docs/Demo_02.java
@@ -0,0 +1,13 @@
+package L7_ForLoop_Lab;
+
+import java.util.Scanner;
+
+public class Demo_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 10;
+        System.out.println(number++);
+        System.out.println(number);
+    }
+}
