52896e71930a693fe7554d47e9585b1465fdae76
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-09 02:26:48 +0200

    Update P01_HelloSoftUni.java

--- JAVA Basics/01. First Steps In Coding/P01_HelloSoftUni.java
+++ JAVA Basics/01. First Steps In Coding/P01_HelloSoftUni.java
@@ -1,9 +1,11 @@
 package L01_FirstStepsInCoding;
 
+import java.util.Scanner;
+
 public class P01_HelloSoftUni {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello SoftUni");
     }
 }
-
