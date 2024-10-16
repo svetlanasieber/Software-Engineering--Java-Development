4cfe323dc0d9c0524860db0263b034ce670c3129
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-10-16 15:47:50 +0200

    Update and rename P04_InchesToCentimeters.java to InchesToCentimeters_04.java

--- JAVA Basics/01. First Steps In Coding/InchesToCentimeters_04.java
+++ JAVA Basics/01. First Steps In Coding/InchesToCentimeters_04.java
@@ -1,8 +1,6 @@
-package L01_FirstStepsInCoding;
-
 import java.util.Scanner;
 
-public class P04_InchesToCentimeters {
+public class InchesToCentimeters_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
